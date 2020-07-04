e183c5e49a69b3648978c98fb28ec1a5e95db2b9
Russell Gapasin <russell@fanswifi.com>
2020-07-05 02:13:35 +0800

    Refactor var variables

--- utils/removeCartItem.js
+++ utils/removeCartItem.js
@@ -1,5 +1,5 @@
 module.exports = (cart, productId) =>{
-  for (var i = 0; i < cart.length; i++) {
+  for (let i = 0; i < cart.length; i++) {
     if (productId == cart[i]._id) cart.splice(i, 1);
   }
   return cart;

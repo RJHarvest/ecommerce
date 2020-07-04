535e762ecee86579df07ffc7efd28762290879fb
Russell Gapasin <russell@fanswifi.com>
2020-07-05 02:04:20 +0800

    Modified config keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,8 @@
+require('dotenv').config()
+
 module.exports = {
-  mongoURI: 'mongodb://russell:russell123@ds151086.mlab.com:51086/gentlemen-watch',
-  cookieKey: 'fdiscunsobesnenwgbe'
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  sessionSecret: process.env.SESSION_SECRET,
+  redisURL: process.env.REDIS_URL,
 }

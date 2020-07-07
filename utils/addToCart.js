0659e24514170b508b1eac823569cedc4e5178b0
Russell Gapasin <russell@fanswifi.com>
2020-07-08 00:12:42 +0800

    Fixed add to cart function

--- utils/addToCart.js
+++ utils/addToCart.js
@@ -1,27 +1,27 @@
 const addPrice = require('./addPrice');
 
-module.exports = (cart, productId, product, quantity) =>{
+const addNewItemToCart = (cart, product, quantity, cost) => {
+  product['quantity'] = quantity;
+  product['cost'] = cost;
+  cart.push(product);
+  return cart;
+}
+
+module.exports = (cart, productId, product, quantity) => {
   if (quantity === 0) return cart;
 
   let cost = 0;
   (quantity > 1) ? cost = addPrice(product, quantity) : cost = product.price;
 
-  if (cart.length === 0) {
-    product['quantity'] = quantity;
-    product['cost'] = cost;
-    cart.push(product);
-    return cart;
+  // increase quantity and cost if item already in cart
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i]._id === productId) {
+      cart[i]['quantity'] += quantity;
+      cart[i]['cost'] += cost;
+      return cart;
+    } 
   }
 
-  const cartItems = cart.map(cartItem => {
-    if (cartItem._id === productId) {
-      return {
-        ...cartItem,
-        quantity: cartItem['quantity'] += quantity,
-        cost: cartItem['cost'] += cost,
-      }
-    }
-  });
-  
-  return cartItems;
+  // append a new item to cart
+  return addNewItemToCart(cart, product, quantity, cost);
 }

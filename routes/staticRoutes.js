cb308c554eb50e7541993e810702b636c889c2d5
Russell Gapasin <russell@fanswifi.com>
2020-07-08 00:16:03 +0800

    Modified and added new static routes

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -7,6 +7,47 @@ module.exports = (app) => {
     res.render('contact', { ...res.locals });
   });
 
+  app.get('/login',
+    (req, res, next) => {
+      if (req.user) {
+        return res.redirect('/member');
+      }
+      return next();
+    },
+    (req, res, next) => {
+    res.render('login', { ...res.locals });
+  });
+
+  app.get('/register',
+    (req, res, next) => {
+      if (req.user) {
+        return res.redirect('back');
+      }
+      return next();
+    },
+    (req, res) => {
+    res.render('register', { ...res.locals });
+  });
+
+  app.get('/member',
+    (req, res, next) => {
+      if (!req.user) {
+        return res.redirect('back');
+      }
+      return next();
+    },
+    (req, res) => {
+      res.render('member', { ...res.locals });
+  });
+
+  app.get('/cart', (req, res) => {
+    res.render('cart', { ...res.locals });
+  });
+
+  app.get('/checkout', (req, res) => {
+    res.render('checkout', { ...res.locals });
+  });
+
   app.get('/return-policy', (req, res) => {
     res.render('returnPolicy', { ...res.locals });
   });
